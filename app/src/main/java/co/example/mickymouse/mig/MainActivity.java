7272b2b197c3a621e1ba8e8d0ac901f0884c598d
Mickymouse3600 <michaelanablarley@gmail.com>
2018-12-27 12:34:17 +0000

    Correction1 Commit

--- app/src/main/java/co/example/mickymouse/mig/MainActivity.java
+++ app/src/main/java/co/example/mickymouse/mig/MainActivity.java
@@ -1,6 +1,7 @@
 package co.example.mickymouse.mig;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +14,15 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +51,25 @@ public class MainActivity extends AppCompatActivity {
         mUploads = new ArrayList<>();
 
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+
+        StorageReference mStoreRef = FirebaseStorage.getInstance().getReference();
+
+        mStoreRef.child("uploads/" + model.getBase64Image())
+                .getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Got the download URL for 'photos/profile.png'
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Toast.makeText(MainActivity.this,"image not dowloaded", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
